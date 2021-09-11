2af439758f8ddce8da3baf3ac56f039f0512c373
Jun-T-git <jun0303t@icloud.com>
2021-09-12 00:16:40 +0900

    その他変更

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,20 @@
 import "~/styles/style.css";
-import "tailwindcss/tailwind.css";
+import Head from "next/head";
 import { AppProps } from "next/app";
 import { ReactElement } from "react";
 
 const MyApp = ({ Component, pageProps }: AppProps): ReactElement => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Slive</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="bg-blue-50 w-screen h-screen">
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 };
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,7 @@
-import Head from "next/head";
-import React from "react";
-import Title from "~/components/title";
+import React, { useEffect, useRef, useState } from "react";
 
 const Index: React.VFC = () => {
-  return (
-    <div>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <div className="text-center px-20 py-10 bg-gray-300">
-          <Title>This is Nextjs Template!</Title>
-          <p className="text-blue-500">Next.js + TypeScript + TailwindCSS</p>
-        </div>
-      </main>
-    </div>
-  );
+  return <div>Index</div>;
 };
 
 export default Index;
