0e37113c04851de86dff4371f4c366ccd69a02ce
Jun-T-git <jun0303t@icloud.com>
2021-09-16 19:44:38 +0900

    ヘッダー追加

---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header: React.VFC = () => {
+  return (
+    <nav className="p-2 md:px-5 border-b border-green-900">
+      <h1 className="text-gray-50 text-3xl font-bold">SLive</h1>
+    </nav>
+  );
+};
+
+export default Header;
