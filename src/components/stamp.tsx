e3e6e35004ce55049fccc29b137765bf309440df
Jun-T-git <jun0303t@icloud.com>
2021-09-16 19:44:59 +0900

    連打防止

--- src/components/stamp.tsx
+++ src/components/stamp.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { postComment } from "~/firebase/comments";
 
 type Props = {
@@ -6,12 +6,25 @@ type Props = {
 };
 
 const Stamp: React.VFC<Props> = ({ code }) => {
+  const [isDisabled, setIsDisabled] = useState<boolean>(false);
+
+  const handleButtonClick = () => {
+    setIsDisabled(true); // 連打防止
+    postComment({ content: `::${code}::`, color: "" });
+    setTimeout(() => setIsDisabled(false), 500);
+  };
+
   return (
     <button
-      onClick={() => postComment({ content: `::${code}::`, color: "" })}
-      className="px-5 py-1 bg-blue-200 rounded-full border border-gray-400 md:hover:opacity-80"
+      onClick={handleButtonClick}
+      disabled={isDisabled}
+      className="px-5 py-1 bg-green-900 rounded-full border border-green-700 md:hover:opacity-80"
     >
-      <img src={`../images/${code}.png`} className="w-5 h-5 mb-0.5" />
+      {isDisabled ? (
+        <img src="../loading.png" className="animate-spin w-5 h-5 mb-0.5" />
+      ) : (
+        <img src={`../stamps/${code}.png`} className="w-5 h-5 mb-0.5" />
+      )}
     </button>
   );
 };
