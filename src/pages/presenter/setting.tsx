d9fce3d1c135b2f8c52f6b6d3b09748659736862
Jun-T-git <jun0303t@icloud.com>
2021-09-16 10:15:15 +0900

    スライドプレビュー機能追加

--- src/pages/presenter/setting.tsx
+++ src/pages/presenter/setting.tsx
@@ -1,10 +1,10 @@
 import React, { ChangeEvent } from "react";
 import Link from "next/link";
-import { useSetRecoilState } from "recoil";
+import { useRecoilState } from "recoil";
 import { slideSrcState } from "~/recoil/atoms";
 
 const Setting: React.VFC = () => {
-  const setSlideSrc = useSetRecoilState(slideSrcState);
+  const [slideSrc, setSlideSrc] = useRecoilState(slideSrcState);
 
   const handleTextareaChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
     const slideLink = e.target.value;
@@ -13,14 +13,34 @@ const Setting: React.VFC = () => {
   };
 
   return (
-    <div className="p-5 mx-auto max-w-md">
+    <div className="p-5 mx-auto max-w-xl">
+      <h1 className="text-center text-2xl font-bold py-5">
+        スライドを設定する
+      </h1>
+      <p className="text-sm py-3">
+        Googleスライドの [ファイル＞ウェブに後悔＞リンク]
+        で表示されるURLをコピーして貼り付けてください。
+      </p>
       <textarea
         className="block w-full p-1 my-1 h-full border text-sm rounded"
         rows={5}
+        placeholder="https://docs.google.com/presentation/..."
         onChange={handleTextareaChange}
       />
-      <Link href="/presenter/presentation">
-        <a className="block text-center w-full py-1 my-5 h-full text-white text-sm rounded bg-blue-500">
+      {slideSrc ? (
+        <iframe src={slideSrc} className="my-10 w-full h-[21rem]" />
+      ) : (
+        <div className="my-10 w-full h-[21rem] bg-gray-100 text-gray-300 text-4xl font-bold flex justify-center items-center border border-gray-300">
+          Slide Preview
+        </div>
+      )}
+      <Link href={slideSrc ? "/presenter/presentation" : "#"}>
+        <a
+          className={
+            "block text-center w-full py-2 my-5 h-full text-white text-sm rounded " +
+            (slideSrc ? "bg-blue-500" : "bg-gray-500")
+          }
+        >
           スライドを設定する
         </a>
       </Link>
